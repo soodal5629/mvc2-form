51ec541a7f1b5dfb0d4eef5d8e7ba6ac86d25005
chaelynjang <chaelynjang@lotte.net>
2022-12-20 21:55:48 +0900

    라이도 버튼 예제

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,5 +1,7 @@
 package hello.itemservice.domain.item;
 
+import lombok.Getter;
+
 public enum ItemType {
 
     BOOK("도서"), FOOD("음식"), ETC("기타");
@@ -9,5 +11,9 @@ public enum ItemType {
     // enum 타입 생성
     ItemType(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -22,7 +23,7 @@ public class FormItemController {
     private final ItemRepository itemRepository;
 
     // 평소에 알고있는 ModelAttribute와 다름
-    // 해당 컨트롤러 호출하는 모든 메소드의 model에 regions를 담아줌!!
+    // 해당 컨트롤러 호출하는 모든 메소드의 model에 regions를 담아줌!! => 중복 코드 방지 가능
     @ModelAttribute("regions")
     public Map<String, String> regions() {
         Map<String, String> regions = new LinkedHashMap<>(); // LinkedHashMap은 순서를 보장해줌
@@ -30,6 +31,12 @@ public class FormItemController {
         regions.put("BUSAN", "부산");
         regions.put("JEJU", "제주");
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() { // enum 타입
+        ItemType[] values = ItemType.values();
+        return values;
     }
 
     @GetMapping
@@ -58,6 +65,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -74,6 +83,7 @@ public class FormItemController {
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        log.info("item.itemType={}", item.getItemType());
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
